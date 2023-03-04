a9c0738154e08a66201e79342c4efdb8c25745f1
MOHAMED HAJJI <pytaek1@gmail.com>
2023-03-04 22:17:22 +0100

    server and http errors handling

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
 
 //dotEnv config
 dotenv.config();
@@ -48,7 +49,22 @@ app.use(
 app.use(cors());
 
 app.post("/test", (req, res) => {
-  res.send(req.body);
+  throw createHttpError.BadRequest("this route has an error");
+});
+
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist."));
+});
+
+//error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import logger from "./configs/logger.config.js";
 //env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server;
+
+server = app.listen(PORT, () => {
   logger.info(`Server is listening at ${PORT}...`);
+});
+
+//handle server errors
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
