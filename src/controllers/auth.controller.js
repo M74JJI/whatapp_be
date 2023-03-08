32ef5db41b45fb5f7cf6d9e2802080d43a18ad2d
MOHAMED HAJJI <pytaek1@gmail.com>
2023-03-08 20:19:16 +0100

    small update for auth controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,13 +32,13 @@ export const register = async (req, res, next) => {
 
     res.json({
       message: "register success.",
-      access_token,
       user: {
         _id: newUser._id,
         name: newUser.name,
         email: newUser.email,
         picture: newUser.picture,
         status: newUser.status,
+        access_token,
       },
     });
   } catch (error) {
@@ -68,13 +68,13 @@ export const login = async (req, res, next) => {
 
     res.json({
       message: "register success.",
-      access_token,
       user: {
         _id: user._id,
         name: user.name,
         email: user.email,
         picture: user.picture,
         status: user.status,
+        access_token,
       },
     });
   } catch (error) {
@@ -106,13 +106,13 @@ export const refreshToken = async (req, res, next) => {
       process.env.ACCESS_TOKEN_SECRET
     );
     res.json({
-      access_token,
       user: {
         _id: user._id,
         name: user.name,
         email: user.email,
         picture: user.picture,
         status: user.status,
+        access_token,
       },
     });
   } catch (error) {
