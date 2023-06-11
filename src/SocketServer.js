a0421bf1e7af3b0f7d49b49a6f73a3aced2e8cac
MOHAMED HAJJI <pytaek1@gmail.com>
2023-06-11 10:22:39 +0100

    video calls

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -9,6 +9,8 @@ export default function (socket, io) {
     }
     //send online users to frontend
     io.emit("get-online-users", onlineUsers);
+    //send socket id
+    io.emit("setup socket", socket.id);
   });
 
   //socket disconnect
@@ -38,5 +40,27 @@ export default function (socket, io) {
   });
   socket.on("stop typing", (conversation) => {
     socket.in(conversation).emit("stop typing");
+  });
+
+  //call
+  //---call user
+  socket.on("call user", (data) => {
+    let userId = data.userToCall;
+    let userSocketId = onlineUsers.find((user) => user.userId == userId);
+    io.to(userSocketId.socketId).emit("call user", {
+      signal: data.signal,
+      from: data.from,
+      name: data.name,
+      picture: data.picture,
+    });
+  });
+  //---answer call
+  socket.on("answer call", (data) => {
+    io.to(data.to).emit("call accepted", data.signal);
+  });
+
+  //---end call
+  socket.on("end call", (id) => {
+    io.to(id).emit("end call");
   });
 }
